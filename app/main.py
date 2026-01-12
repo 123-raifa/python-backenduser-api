a3b22eb2ddb810ae852c1c443b98208db8892ea0
123-raifa <raifafatima970@gmail.com>
2026-01-12 19:46:24 +0530

    Add user creation API with database

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
+from app.database import engine
 from fastapi import FastAPI
+from app.database import engine
+from app.models import Base
 
 app = FastAPI()
 
@@ -14,4 +17,7 @@ class NameRequest(BaseModel):
 def greet_user(data: NameRequest):
     return {"message": f"Hello {data.name}"}
 
+@app.on_event("startup")
+def start_db():
+    Base.metadata.create_all(bind=engine)
 
