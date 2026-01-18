a66f3e4fd700e2b065bfb4058dcbcfb9b4ff12d4
123-raifa <raifafatima970@gmail.com>
2026-01-18 20:11:38 +0530

    Refactor user APIs into routes for clean project structure

--- app/main.py
+++ app/main.py
@@ -6,8 +6,12 @@ from sqlalchemy.orm import Session
 from app.models import User
 from app.schemas import UserCreate
 from app.schemas import UserUpdate
+from app.routes.users import router as users_router
+
 
 app = FastAPI()
+app.include_router(users_router)
+
 
 @app.get("/hello")
 def say_hello():
@@ -25,67 +29,5 @@ def greet_user(data: NameRequest):
 def start_db():
     Base.metadata.create_all(bind=engine)
 
-@app.post("/users")
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
 
-    # 1️⃣ check if email already exists
-    existing_user = db.query(User).filter(User.email == user.email).first()
-    if existing_user:
-        raise HTTPException(
-            status_code=400,
-            detail="Email already registered"
-        )
-
-    # 2️⃣ create new user
-    new_user = User(name=user.name, email=user.email)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
-
-@app.get("/users")
-def get_users(db: Session = Depends(get_db)):
-    return db.query(User).all()
-
-@app.delete("/users/{user_id}")
-def delete_user(user_id: int, db: Session = Depends(get_db)):
-    
-    user = db.query(User).filter(User.id == user_id).first()
-    
-    if not user:
-        raise HTTPException(
-            status_code=404,
-            detail="User not found"
-        )
-    
-    db.delete(user)
-    db.commit()
-    
-    return {"message": "User deleted successfully"}
-
-@app.patch("/users/{user_id}")
-def update_user(
-    user_id: int,
-    user: UserUpdate,
-    db: Session = Depends(get_db)
-):
-    existing_user = db.query(User).filter(User.id == user_id).first()
-
-    if not existing_user:
-        raise HTTPException(
-            status_code=404,
-            detail="User not found"
-        )
-
-    if user.name is not None:
-        existing_user.name = user.name
-
-    if user.email is not None:
-        existing_user.email = user.email
-
-    db.commit()
-    db.refresh(existing_user)
-
-    return existing_user
 
